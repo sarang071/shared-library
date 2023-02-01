876bbe0e25939c4d8e947d728945516b8ccb6f1b
sarang071 <121010297+sarang071@users.noreply.github.com>
2023-02-01 15:59:48 +0530

    Update DockerDeliveryPipeline.groovy

--- vars/DockerDeliveryPipeline.groovy
+++ vars/DockerDeliveryPipeline.groovy
@@ -3,21 +3,37 @@
 def config = [name: 'Newman', dayOfWeek: 'Friday']
 
 pipeline {
-
     agent any
-
+    environment {
+        dev_account_id = "1245789"
+        qa_account_id  = "5478951"
+    }
+    parameters {
+        choice(name: 'ACCOUNT', choices: ['dev','qa'], description: 'Pick Enviornment')
+    }
     stages {
-
-        stage('Example') {
-
-            steps {
-
-                helloWorld(config)
+        stage('dev account'){
+                when{
+                    expression{
+                        params.ACCOUNT == 'dev'
+                    }
+                }
+                steps{
+                    echo "We are using ${env.dev_account_id}"
+                }
 
             }
+        stage('QA account'){
+                when{
+                    expression{
+                        params.ACCOUNT == 'qa'
+                    }
+                }
+                steps{
+                    echo "We are using ${env.qa_account_id}"
+                }
 
+            }
         }
-
-    }
 
 }
